a87b6120eab1a79cbcbf97223141dde9f40cdff2
Emmanuel Bonnet <emmanuel.bonnet@inukshuk.io>
2019-04-24 11:14:39 +0200

    initial commit with standard java archetype

---
+++ kie_monitor_rest/src/test/java/io/inukshuk/redhat/drools/utils/AppTest.java
@@ -0,0 +1,20 @@
+package io.inukshuk.redhat.drools.utils;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
